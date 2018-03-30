dd984b321c81cb45711682f20f00994664d90e0c
dsdunn <dsdunnmusic@gmail.com>
2018-03-30 16:07:33 -0600

    Add html skeleton and css styling

---
+++ index.js
@@ -0,0 +1,6 @@
+var ideaInput = document.querySelector('#title');
+var bodyInput = document.querySelector('#body');
+var saveButton = document.querySelector('#save-button');
+var searchInput = document.querySelector('#search-box');
+
+saveButton.onclick(createNewCard());
