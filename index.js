1c1b934563ec8959158addeabe50319666de89d6
David Stonecipher <stonecipherdr@gmail.com>
2018-04-01 12:35:56 -0600

    Add functions for idea generation and field reset

--- index.js
+++ index.js
@@ -4,3 +4,15 @@ var saveButton = document.querySelector('#save-button');
 var searchInput = document.querySelector('#search-box');
 
 saveButton.onclick(createNewCard());
+
+function newIdea (title, body, id, quality = 'swill') {
+    this.title = title,
+    this.body = body,
+    this.id = id,
+    this.quality = quality;
+}
+
+function clearInputs() {
+    ideaInput.value = '';
+    bodyInput.value = '';
+}
