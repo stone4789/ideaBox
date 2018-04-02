021abf6e75b26a0baf4e140539af4e663046edd6
dsdunn <dsdunnmusic@gmail.com>
2018-04-02 17:58:48 -0600

    Add idea array and implement local storage

--- index.js
+++ index.js
@@ -3,40 +3,88 @@ var bodyInput = document.querySelector('#body');
 var saveButton = document.querySelector('#save-button');
 var searchInput = document.querySelector('#search-box');
 var ideaList = document.querySelector('#idea-space')
-var ideaCount = 0;
+var ideaArray = [];
 
 saveButton.addEventListener('click',createNewCard);
- 
+window.addEventListener('load', getStorage);
+
+
+document.querySelector('.upvote').addEventListener('click',updateQuality(1));
+document.querySelector('#idea-space').addEventListener('click',updateQuality(-1));
+
+
+
+function store(arr) {
+  localStorage.setItem('data',JSON.stringify(arr));
+}
+
+function getStorage(){
+  ideaArray = JSON.parse(localStorage.getItem('data'));
+  ideaArray.forEach(function(obj){
+    renderCard(obj);
+  });
+}
+
 function Card(title,body,id) {
   this.title = title;
   this.body = body;
   this.quality = "swill";
+  this.numQuality = 0;
   this.id = id;
 }
 
 Card.prototype.updateQuality = function(n) {
+
+  event.target 
   if (this.quality === "swill" && n === 1) {
     this.quality = "decent";
+    this.numQuality = 1;
   } else if (this.quality === "decent" && n === 1) {
-    this.quality === "excellent";
+    this.quality = "excellent";
+    this.numQuality = 2
   }else if (this.quality === "excellent" && n === -1) {
-    this.quality === "decent";
+    this.quality = "decent";
+    this.numQuality = 1;
   }else if (this.quality === "decent" && n === -1) {
     this.quality = "swill";
+    this.numQuality = 0;
   }
+  sortCards();
+}
+
+function sortCards() {
+  ideaArray.sort(function (a,b){
+    if (a.numQuality > b.numQuality) {
+      return 1;
+    } else {
+      return -1;
+    }
+  })
+  ideaArray.forEach(function(obj){
+    renderCard(obj);
+  });
+  store(ideaArray);
 }
 
 function createNewCard(e) {
   e.preventDefault();
-
   var idea = ideaInput.value;
   var body = bodyInput.value;
-  var idNumber = ideaCount;
-  var newCard = new Card(idea,body,idNumber);
+  var idNumber = Date.now();
+  var cardObject = new Card(idea,body,idNumber);
+  ideaArray.push(cardObject);
+  console.log(ideaArray);
+  store(ideaArray);
+  renderCard(cardObject);
+}
+
+function renderCard(newCard) {
 
   var card = document.createElement('li');
   card.innerHTML = 
-    '<li class="idea-card">' +
+    '<li id= "n' +
+    newCard.idNumber +
+    '" class="idea-card">' +
     '<h2 class="card-title">' + 
       newCard.title + 
     '</h2>' +
@@ -62,11 +110,12 @@ function createNewCard(e) {
 
   ideaList.appendChild(card);
   document.querySelector('form').reset();
-  //clearInputs();
+}
+
   //addToLocalStorage();
 
 
-}
+
 
 
 
